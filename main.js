288aa721ee0a1b82b4c7ccc48e12bd9ce3da389b
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-12-09 11:47:07 +0100

    basic js

---
+++ main.js
@@ -0,0 +1,124 @@
+
+const modalOne = document.getElementById("modal1");
+const arrowOne = document.getElementById("a1");
+const lightOne = document.getElementById("light1");
+const invisibleOne = document.getElementById("i1");
+
+modalOne.addEventListener('click', () => {
+    invisibleOne.style.display = "flex";
+    lightOne.style.fontWeight = "700";
+    arrowOne.style.transform = "rotate(180deg)";
+
+    invisibleTwo.style.display = "none";
+    lightTwo.style.fontWeight = "400";
+    arrowTwo.style.transform = "rotate(360deg)";
+    invisibleThree.style.display = "none";
+    lightThree.style.fontWeight = "400";
+    arrowThree.style.transform = "rotate(360deg)";
+    invisibleFour.style.display = "none";
+    lightFour.style.fontWeight = "400";
+    arrowFour.style.transform = "rotate(360deg)";
+    invisibleFive.style.display = "none";
+    lightFive.style.fontWeight = "400";
+    arrowFive.style.transform = "rotate(360deg)";
+})
+
+
+const modalTwo = document.getElementById("modal2");
+const arrowTwo = document.getElementById("a2");
+const lightTwo = document.getElementById("light2");
+const invisibleTwo = document.getElementById("i2");
+
+modalTwo.addEventListener('click', () => {
+    invisibleTwo.style.display = "flex";
+    lightTwo.style.fontWeight = "700";
+    arrowTwo.style.transform = "rotate(180deg)";
+
+    invisibleOne.style.display = "none";
+    lightOne.style.fontWeight = "400";
+    arrowOne.style.transform = "rotate(360deg)";
+    invisibleThree.style.display = "none";
+    lightThree.style.fontWeight = "400";
+    arrowThree.style.transform = "rotate(360deg)";
+    invisibleFour.style.display = "none";
+    lightFour.style.fontWeight = "400";
+    arrowFour.style.transform = "rotate(360deg)";
+    invisibleFive.style.display = "none";
+    lightFive.style.fontWeight = "400";
+    arrowFive.style.transform = "rotate(360deg)";
+})
+
+
+const modalThree = document.getElementById("modal3");
+const arrowThree = document.getElementById("a3");
+const lightThree = document.getElementById("light3");
+const invisibleThree = document.getElementById("i3");
+
+modalThree.addEventListener('click', () => {
+    invisibleThree.style.display = "flex";
+    lightThree.style.fontWeight = "700";
+    arrowThree.style.transform = "rotate(180deg)";
+
+    invisibleOne.style.display = "none";
+    lightOne.style.fontWeight = "400";
+    arrowOne.style.transform = "rotate(360deg)";
+    invisibleTwo.style.display = "none";
+    lightTwo.style.fontWeight = "400";
+    arrowTwo.style.transform = "rotate(360deg)";
+    invisibleFour.style.display = "none";
+    lightFour.style.fontWeight = "400";
+    arrowFour.style.transform = "rotate(360deg)";
+    invisibleFive.style.display = "none";
+    lightFive.style.fontWeight = "400";
+    arrowFive.style.transform = "rotate(360deg)";
+})
+
+
+const modalFour = document.getElementById("modal4");
+const arrowFour = document.getElementById("a4");
+const lightFour = document.getElementById("light4");
+const invisibleFour = document.getElementById("i4");
+
+modalFour.addEventListener('click', () => {
+    invisibleFour.style.display = "flex";
+    lightFour.style.fontWeight = "700";
+    arrowFour.style.transform = "rotate(180deg)";
+
+    invisibleOne.style.display = "none";
+    lightOne.style.fontWeight = "400";
+    arrowOne.style.transform = "rotate(360deg)";
+    invisibleTwo.style.display = "none";
+    lightTwo.style.fontWeight = "400";
+    arrowTwo.style.transform = "rotate(360deg)";
+    invisibleThree.style.display = "none";
+    lightThree.style.fontWeight = "400";
+    arrowThree.style.transform = "rotate(360deg)";
+    invisibleFive.style.display = "none";
+    lightFive.style.fontWeight = "400";
+    arrowFive.style.transform = "rotate(360deg)";
+})
+
+
+const modalFive = document.getElementById("modal5");
+const arrowFive = document.getElementById("a5");
+const lightFive = document.getElementById("light5");
+const invisibleFive = document.getElementById("i5");
+
+modalFive.addEventListener('click', () => {
+    invisibleFive.style.display = "flex";
+    lightFive.style.fontWeight = "700";
+    arrowFive.style.transform = "rotate(180deg)";
+
+    invisibleOne.style.display = "none";
+    lightOne.style.fontWeight = "400";
+    arrowOne.style.transform = "rotate(360deg)";
+    invisibleTwo.style.display = "none";
+    lightTwo.style.fontWeight = "400";
+    arrowTwo.style.transform = "rotate(360deg)";
+    invisibleThree.style.display = "none";
+    lightThree.style.fontWeight = "400";
+    arrowThree.style.transform = "rotate(360deg)";
+    invisibleFour.style.display = "none";
+    lightFour.style.fontWeight = "400";
+    arrowFour.style.transform = "rotate(360deg)";
+})
